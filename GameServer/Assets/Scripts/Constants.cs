8c47063ce3e993855cd3974722c82ecd8971deb3
Fabiano Evaristo <gameplaysfabiano@gmail.com>
2022-03-20 19:11:35 -0300

    GameServer Fix

--- GameServer/Assets/Scripts/Constants.cs
+++ GameServer/Assets/Scripts/Constants.cs
@@ -10,6 +10,7 @@ namespace GameServer
         public const float MS_PER_TICK = 1000f / TICKS_PER_SEC;
         public const float DelayReceiveMovement = 0.1f;
         public const bool Test = true;
+        public const bool AutoCreateAccount = true;
         public const int MinPlayer_RoomStart = 1;
         //
         public const int SIO_UDP_CONNRESET = -1744830452;
